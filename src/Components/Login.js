80bc7460118b5a04f31d051a72860e1c7563e9c5
Jersabell <jersabell@gmail.com>
2022-03-23 19:02:26 -0500

    Cambios 23/03

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,11 +3,13 @@ import { Register } from './Register.js';
 
 export const Login = () => {
   document.getElementById('root').innerHTML = `
+  
+  <div id="divIngresar">
   <div id="divLogo">
   <img src="images/logo-fit-women.png" class="logo"/>
   </div>
-  <div id="divIngresar">
   <div class="ingresarE-mail">
+  <br>
   <i class="fa-solid fa-envelope e-mailIcon"></i>
   <input type="email" class="e-mail" id="correo" placeholder="Correo electrónico"></input><br><br>
   </div>
@@ -15,11 +17,12 @@ export const Login = () => {
   <i class="fa-solid fa-lock passwordIcon"></i>
   <input type=password class="password" id="contraseña" placeholder="Contraseña"></input><br><br>
   </div>
-  <button id="iniciar">Iniciar sesión</button> <br><br><br><br><br>
+  <button id="iniciar">Iniciar sesión</button> <br><br>
   <button id="google"><img src="images/google-img.png" class="imageGoogle"/><p>Continuar con Google</p></button> <br><br>
   <p id="o">ó</p><br>
   <p id="registro">Registrate en FitWoman</p><br><br>
-  </div>`;
+  </div>
+  <div class="div__imageLogin"><img src="images/fondoInicio3.webp" class="imageLogin"/></div>`;
 
   // Función para carga de registro
   const scrRegister = document.querySelector('#registro');
