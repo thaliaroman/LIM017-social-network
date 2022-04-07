9cdc128f6dac176ace67227681c8db719a10dffc
Jersabell <jersabell@gmail.com>
2022-04-06 20:33:48 -0500

    desacoplando functions

--- src/lib/controllers.js
+++ src/lib/controllers.js
@@ -1,9 +1,12 @@
 /* eslint-disable arrow-body-style */
 import {
   // eslint-disable-next-line import/named
-  registerUser, loginUser, getCurrentUser,
+  registerUser,
+  loginUser,
+  updater, sendMail, startGoogle, Provider, loginOutUser, toPost, loadPosts,
 } from './libraries-Firebase.js';
-// registra nuevos usuarios
+
+// Registra nuevos usuarios
 export const register = () => {
   const email = document.getElementById('e-mail').value;
   const password = document.getElementById('password').value;
@@ -11,8 +14,11 @@ export const register = () => {
   const fullName = document.getElementById('name').value;
   if (password === confirmPassword) {
     registerUser(email, password, fullName)
-      .then(() => {
+      .then((userCredential) => {
+        updater(fullName);
+        sendMail();
         window.location.hash = '/login';
+        return userCredential.user;
       })
       .catch((error) => {
         const errorCode = error.code;
@@ -22,19 +28,20 @@ export const register = () => {
       });
   } else {
     // eslint-disable-next-line no-alert
-    alert('no coincide la contraseña');
+    alert('No coincide la contraseña');
   }
 };
 
-// logea usuarios registrados
+// Logea usuarios registrados
 export const login = () => {
   const email = document.getElementById('e-mailLogin').value;
   const password = document.getElementById('passwordLogin').value;
   loginUser(email, password)
-    .then(() => {
+    .then((userCredential) => {
       // Signed in
       window.location.hash = '#/home';
       // ...
+      return userCredential.user;
     })
     .catch((error) => {
       const errorCode = error.code;
@@ -44,38 +51,59 @@ export const login = () => {
     });
 };
 
-// Iniciar sesión con Google
-// export const loginGoogle = () => {
-//   return startGoogle
-//     .then(() => {
-//       // Signed in
-//       window.location.hash = '#/home';
-//     })
-//     .catch((error) => {
-//       // Handle Errors here.
-//       const errorCode = error.code;
-//       const errorMessage = error.message;
-//       // The email of the user's account used.
-//       const email = error.email;
-//       // The AuthCredential type that was used.
-//       const credential = GoogleAuthProvider.credentialFromError(error);
-//       // ...
-//     });
-// };
+// Inicia sesión con Google
+export const loginGoogle = () => {
+  startGoogle(new Provider())
+    .then(() => {
+      window.location.hash = '#/home';
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = Provider.credentialFromError(error);
+      // ...
+    });
+};
 
-export const getCurrent = () => getCurrentUser('anonimo');
+// Cierra sesión
+export const loginOut = () => {
+  loginOutUser().then(() => {
+    window.location.hash = '#/login';
+  // Sign-out successful.
+  }).catch((error) => {
+  // An error happened.
+  });
+};
 
-// crea colección
-// Crear un documento con el contenido a publicar en la colección publicaciones
-// export const toPoster = () => {
-//   const forPost = document.querySelector('.post__input').value;
-//   const user = getCurrent();
+// Crea un documento en la coleccion de firestore
+export const toPostDocument = () => {
+  // const user = auth.currentUser;
+  const forPost = document.querySelector('.post__input').value;
+  return toPost(forPost);
+};
 
-//   const dateTime = toTheTime();
-//   // const content = forPost;
-//   const photo = user.photoURL;
-
-//   const docRef = toPost(user.displayName, user.uid, dateTime, forPost, photo);
-//   console.log('Document written with ID: ', docRef.id);
-//   return docRef;
-// };
+// publica el post
+export const publica = () => {
+  function idk(querySnapshot) {
+    const containerPost = document.querySelector('.main__div-postPeople');
+    let html = '';
+    querySnapshot.forEach((doc) => {
+      // console.log(doc.data());
+      const dataDoc = doc.data();
+      html += `
+    <section class="main__section-postPeople" id="">
+        <h3>${dataDoc.user}.</h3>
+        <p id="postHour">Publicado a las: ${dataDoc.dateTime.toDate()}</p>
+        <p>${dataDoc.content}</p>
+        <figure>
+          <img class="post2Img" src="../images/foto-post.jpg">
+        </figure>
+      </section>`;
+    });
+    containerPost.innerHTML = html;
+  }
+  loadPosts(idk);
+};
--- src/lib/libraries-Firebase.js
+++ src/lib/libraries-Firebase.js
@@ -26,125 +26,59 @@ import { app } from './configurationfirebase.js';
 const db = getFirestore();
 const auth = getAuth();
 
+export const updater = (fullName) => updateProfile(auth.currentUser, {
+  displayName: fullName,
+});
+
+export const sendMail = () => sendEmailVerification(auth.currentUser);
+
 // Función para registrarse con correo y contraseña
 export const registerUser = (email, password, fullName) => {
   auth.languageCode = 'es';
-  return createUserWithEmailAndPassword(auth, email, password, fullName)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      console.log(user);
-      updateProfile(user, {
-        displayName: fullName,
-      });
-      sendEmailVerification(auth.currentUser)
-        .then(() => {
-          // Email verification sent!
-          // ...
-        });
-      console.log(userCredential);
-      return userCredential;
-    });
+  return createUserWithEmailAndPassword(auth, email, password);
 };
 
 // Función para iniciar sesión - usuarios registrados
 export const loginUser = (email, password) => {
-  return signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      // ...
-      return user;
-    });
+  return signInWithEmailAndPassword(auth, email, password);
 };
 
 // Iniciar sesión con Google
-export const startGoogle = () => {
-  const provider = new GoogleAuthProvider();
-  signInWithPopup(auth, provider)
-    .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      // ...
-      // googleUsers();
-      window.location.hash = '#/home';
-    }).catch((error) => {
-    // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-    });
+export const Provider = GoogleAuthProvider;
+export const startGoogle = (prov) => {
+  return signInWithPopup(auth, prov);
 };
 
 // Funcion para obtener la información del perfil del usuario logeado
-export const getCurrentUser = (unknow) => {
+export const getCurrentUser = () => {
+  const unknow = 'unknow';
   const user = auth.currentUser;
   if (user !== null) {
-    console.log(`estoy mostrando el usurario: ${user.displayName}`);
     return user;
   }
   return { displayName: unknow };
 };
 
 // Crear un documento con el contenido a publicar en la colección publicaciones
-// export const toPost = (user1, uid1, dateTime1, content1, photo1) => addDoc(collection(db, 'publicaciones'), {
-//   user: user1, uid: uid1, dateTime: dateTime1, content: content1, photo: photo1,
-// });
-// export const toTheTime = () => Timestamp.fromDate(new Date());
-// Crear un documento con el contenido a publicar en la colección publicaciones
-export const toPost = async () => {
-  const user = auth.currentUser;
-  const forPost = document.querySelector('.post__input').value;
-  const docRef = await addDoc(collection(db, 'publicaciones'), {
+export const toPost = (forPost) => {
+  const docRef = addDoc(collection(db, 'publicaciones'), {
     user: getCurrentUser().displayName,
-    uid: user.uid,
+    uid: getCurrentUser().uid,
     dateTime: Timestamp.fromDate(new Date()),
     content: forPost,
-    photo: user.photoURL,
+    photo: getCurrentUser().photoURL,
   });
-  console.log("Document written with ID: ", docRef.id);
   return docRef;
 };
 
 // Leer el contenido de cada documento de la colección publicaciones
-export const loadPosts = async () => {
+export const loadPosts = async (idk) => {
   const q = query(collection(db, 'publicaciones'), orderBy('dateTime', 'desc'));
   // onSnapshot para actualización de datos en timpo real
-  const publishSnapshot = onSnapshot(q, (querySnapshot) => {
-    let html = '';
-    const containerPost = document.querySelector('.main__div-postPeople');
-    querySnapshot.forEach((doc) => {
-      // console.log(doc.data());
-      const dataDoc = doc.data();
-      html += `
-      <section class="main__section-postPeople" id="">
-          <h3>${dataDoc.user}.</h3>
-          <p id="postHour">Publicado a las: ${dataDoc.dateTime.toDate()}</p>
-          <p>${dataDoc.content}</p>
-          <figure>
-            <img class="post2Img" src="../images/foto-post.jpg">
-          </figure>
-        </section>`;
-    });
-    containerPost.innerHTML = html;
-  });
+  await onSnapshot(q, idk);
 };
 
 // Funcion para cerrar sesión
 export const loginOutUser = () => {
-  const auth = getAuth();
-  signOut(auth).then(() => {
-    window.location.hash = '#/login';
-  // Sign-out successful.
-  }).catch((error) => {
-  // An error happened.
-  });
+  return signOut(auth);
 };
-
-
